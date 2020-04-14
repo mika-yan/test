da10df0da79f85d8112767331ead63914972d99b
Альберт <albertmik1915@gmail.com>
2020-04-14 14:19:27 +0300

    Updates

---
+++ precache-manifest.ef1aa72bab6c711cee5cb4b8df990be5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03950762ccf3d18f0163b419b7d1b91",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "4e050035e139ff85942c",
+    "url": "/test/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "938baa22c87491492c17",
+    "url": "/test/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4e050035e139ff85942c",
+    "url": "/test/static/js/2.495f7809.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/test/static/js/2.495f7809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938baa22c87491492c17",
+    "url": "/test/static/js/main.da5573eb.chunk.js"
+  },
+  {
+    "revision": "ebe67c67e4b3851022f0",
+    "url": "/test/static/js/runtime-main.0b54e0ba.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  }
+]);
